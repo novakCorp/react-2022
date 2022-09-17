b3eb9d187797b3cc124fde85bd2640bc46cee80d
nova <nova.corps1010@outlook.com>
2022-09-17 17:33:29 -0400

    Inital commit of the project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world, a new app is coming to save the world</h1>
+    </div>
+  );
+}
+
+export default App;
